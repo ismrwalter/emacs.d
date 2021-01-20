ec5616d50628d1a75277c23a927101de2ae0fb40
Maf <wmafire@gmail.com>
2021-01-21 03:23:38 +0800

    .

--- lisp/major-mode/org.el
+++ lisp/major-mode/org.el
@@ -5,16 +5,7 @@
 (use-package
   org
   :ensure org-plus-contrib
-  :custom-face          ;
-  ;; (org-block ((t
-  ;;              (:background nil
-  ;;                           :slant italic))))
-  ;; (org-block-begin-line ((t
-  ;;                         (:background nil
-  ;;                                      :slant italic))))
-  ;; (org-block-end-line ((t
-  ;;                       (:background nil
-  ;;                                    :slant italic))))
+  :custom-face                          ;
   :config                               ;
   (require 'ob-dot)
   ;; (require 'ob-plantuml)
@@ -69,24 +60,31 @@
   :custom                               ;
   (org-roam-directory "~/Storage/Nutstore/Notes")
   (org-roam-index-file "Index.org")
+  (org-roam-dailies-directory "Daily")
   (org-roam-title-sources '((title headline) alias))
-  (org-roam-tag-sources '(vanilla))
+  (org-roam-tag-sources '(vanilla all-directories))
+  (org-roam-capture-templates '(("d" "default" plain #'org-roam-capture--get-point "%?"
+                                 :file-name "%<%y%m%d%H%M%S>-${slug}"
+                                 :head "* ${title} \n"
+                                 :unnarrowed t)))
+  (org-roam-capture-immediate-template '("d" "default" plain #'org-roam-capture--get-point "%?"
+                                         :file-name "%<%y%m%d%H%M%S>-${slug}"
+                                         :head "* ${title} \n"
+                                         :unnarrowed t))
+  (org-roam-dailies-capture-templates '(("d" "default" entry #'org-roam-capture--get-point "* %?"
+                                         :file-name "Daily/%<%Y-%m-%d>"
+                                         :head "* %<%A, %d %B %Y> \n")))
   :config                               ;
-  (w/create-leader-key "d" 'org-roam-jump-to-index "index" major-map-prefix)
-  (w/create-leader-key-for-mode 'org-mode "n" 'org-roam "backlink" major-map-prefix)
-  (w/create-leader-key-for-mode 'org-mode "f" 'org-roam-find-file "find-file" major-map-prefix)
-  (w/create-leader-key-for-mode 'org-mode "n" 'org-roam-graph "graph" major-map-prefix)
-  (w/create-leader-key-for-mode 'org-mode "i" 'org-roam-insert "insert" major-map-prefix)
-  (w/create-leader-key-for-mode 'org-mode "I" 'org-roam-insert-immediate "insert-immediate" major-map-prefix)
-  (require 'org-roam-protocol)
-  (setq org-roam-capture-templates '(("d" "default" plain #'org-roam-capture--get-point "%?"
-                                      :file-name "${slug}-%<%y%m%d%H%M%S>"
-                                      :head "* ${title} \n"
-                                      :unnarrowed t)))
-  (setq org-roam-capture-immediate-template '("d" "default" plain #'org-roam-capture--get-point "%?"
-                                              :file-name "${slug}-%<%y%m%d%H%M%S>"
-                                              :head "* ${title} \n"
-                                              :unnarrowed t)))
+  (setq note-map-prefix (w/create-leader-keymap "n" note-mode-map "note"))
+  (w/create-leader-key "n" 'org-roam-jump-to-index "index" note-map-prefix)
+  (w/create-leader-key "d" 'org-roam-dailies-find-today "today" note-map-prefix)
+  (w/create-leader-key "f" 'org-roam-find-file "find-file" note-map-prefix)
+  (w/create-leader-key-for-mode 'org-mode "l" 'org-roam "backlink" note-map-prefix)
+  (w/create-leader-key-for-mode 'org-mode "g" 'org-roam-graph "graph" note-map-prefix)
+  (w/create-leader-key-for-mode 'org-mode "i" 'org-roam-insert "insert" note-map-prefix)
+  (w/create-leader-key-for-mode 'org-mode "I" 'org-roam-insert-immediate "insert-immediate"
+                                note-map-prefix)
+  (require 'org-roam-protocol))
 (use-package
   deft
   :ensure t
@@ -109,6 +107,44 @@
                 org-roam-server-network-label-truncate-length 60
                 org-roam-server-network-label-wrap-length 20))
 
+(setq org-agenda-files '("~/Storage/Nutstore/Notes/Daily/Agenda.org"))
+;; (setq org-agenda-time-grid (quote ((daily today require-timed)
+;;                                    (300 600 900 1200 1500 1800 2100 2400) "......"
+;;                                    "-----------------------------------------------------")))
+;; (setq org-agenda-include-diary t)
+;; (setq org-agenda-diary-file (expand-file-name "standard-diary" user-emacs-directory)) ;;2020-03-02 10:47:06
+;; (setq diary-file (expand-file-name "standard-diary" user-emacs-directory))
+;; (setq calendar-latitude 40.0024) ;;lat, flat
+;; (setq calendar-longitude 116.2962) ;;long是经度
+;; ;;Sunrise and Sunset
+;; ;;日出而作, 日落而息
+;; (defun diary-sunrise ()
+;;   (let ((dss (diary-sunrise-sunset)))
+;;     (with-temp-buffer
+;;       (insert dss)
+;;       (goto-char (point-min))
+;;       (while (re-search-forward " ([^)]*)" nil t)
+;;         (replace-match "" nil nil))
+;;       (goto-char (point-min))
+;;       (search-forward ",")
+;;       (buffer-substring (point-min) (match-beginning 0)))))
+
+;; (defun diary-sunset ()
+;;   (let ((dss (diary-sunrise-sunset))
+;;         start end)
+;;     (with-temp-buffer
+;;       (insert dss)
+;;       (goto-char (point-min))
+;;       (while (re-search-forward " ([^)]*)" nil t)
+;;         (replace-match "" nil nil))
+;;       (goto-char (point-min))
+;;       (search-forward ", ")
+;;       (setq start (match-end 0))
+;;       (search-forward " at")
+;;       (setq end (match-beginning 0))
+;;       (goto-char start)
+;;       (capitalize-word 1)
+;;       (buffer-substring start end))))
 
 (provide 'major-mode/org)
 ;;; org-config.el ends here
